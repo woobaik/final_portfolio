bbbc5406bc7eca902ddfe22c3ad39430e6b8ee08
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-08-05 00:01:04 -0700

    Basic setup for portfolio controller

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,11 +1,24 @@
 class PortfolioController < ApplicationController
+
+  before_action :find_portfolio, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @portfolios = Portfolio.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @portfolios }
+    end
   end
 
   def new
+    @portfolio = Portfolio.new
   end
 
   def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+
   end
 
   def show
@@ -18,5 +31,16 @@ class PortfolioController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def find_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:name, :description)
   end
 end
